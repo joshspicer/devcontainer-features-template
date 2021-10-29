8032019a7923d48ef1fefcd3988b8126b7c5b6d1
Josh Spicer <hello@joshspicer.com>
2021-10-29 10:53:47 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,13 +4,14 @@ set -e
 # The install.sh script is the installation entrypoint for any features in this repository. 
 #
 # The tooling will parse the features.json + user devcontainer, and write 
-# any build-time arguments into an .env file with the format
-# _USERNAME_REPONAME_FEATUREID_OPTION=value
+# any build-time arguments into a feature-set scoped "features.env"
+# The author is free to source that file and use it however they would like.
 set -a
 . ./features.env
 set +a
 
-GREETING=${_JOSHSPICER_DEVCONTAINER_FEATURES_TEMPLATE_HELLOWORLD_GREETING:-undefined}
+# Build args are exposed to this entire feature set following the pattern:  _BUILD_ARG_<FEATURE ID>_<OPTION NAME>
+GREETING=${_BUILD_ARG_HELLOWORLD_GREETING:-undefined}
 
 tee /usr/hello.sh > /dev/null \
 << EOF
